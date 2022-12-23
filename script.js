1cbb37194bdb736bf5cc292eef5c1d5e665d42d9
Alan Pason <alan.pason132@gmail.com>
2022-12-23 13:06:21 +0100

    Add some pseudocode

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("it worked");
+// choose mode
+//   take all buttons
+//   if(this== color mode)
+//      take value from colors
+//      give him class active
+//      change color to picked
+//   if(this== rainbow mode)
+//      create a random color
+//      give him class active
+//      change color to random
+//   if(this== eraser)
+//      give him class active
+//      change color to white
+//   if(this== clear)
+//      clear board
+
+// selecting the table size
+//  when valueofinput change
+// set value of number-col-row in class
+// remove all divs
+// add divs
+
+//drawing
+// when clicked and push
+//  divs change to chosen color
+//
